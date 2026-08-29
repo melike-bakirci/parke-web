5dd288ee2f620044e2f7024f3a15fbdc68e9c2c1
melike-bakirci <melike.bakirci@outlook.com>
2026-08-29 16:14:26 +0300

    uygulama galerisi görsel isimleri güncelledi

--- src/data/references.ts
+++ src/data/references.ts
@@ -19,9 +19,10 @@ export const partnerCompanies = [
 
 export const galleryImages = [
 	{ id: 1, img: "/images/uygulama-galerisi/uygulama-galerisi-1.webp", alt: "Parke Zemin Uygulaması" },
+	{ id: 2, img: "/images/uygulama-galerisi/uygulama-galerisi-2.webp", alt: "Parke Zemin Uygulaması" },
+	{ id: 3, img: "/images/uygulama-galerisi/uygulama-galerisi-3.webp", alt: "Parke Zemin Uygulaması" },
+	{ id: 4, img: "/images/uygulama-galerisi/uygulama-galerisi-4.webp", alt: "Parke Zemin Uygulaması" },
 	{ id: 5, img: "/images/uygulama-galerisi/uygulama-galerisi-5.webp", alt: "Parke Zemin Uygulaması" },
-	{ id: 6, img: "/images/uygulama-galerisi/uygulama-galerisi-6.webp", alt: "Parke Zemin Uygulaması" },
-	{ id: 14, img: "/images/uygulama-galerisi/uygulama-galerisi-14.webp", alt: "Parke Zemin Uygulaması" },
-	{ id: 15, img: "/images/uygulama-galerisi/uygulama-galerisi-15.webp", alt: "Parke Zemin Uygulaması" },
-	{ id: 16, img: "/images/uygulama-galerisi/uygulama-galerisi-16.webp", alt: "Parke Zemin Uygulaması" }
+	{ id: 6, img: "/images/uygulama-galerisi/uygulama-galerisi-6.webp", alt: "Parke Zemin Uygulaması" }
 ];
+
