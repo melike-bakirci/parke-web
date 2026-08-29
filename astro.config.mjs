144cac644dd89092bacceeb7557909777fa7d7e2
melike-bakirci <melike.bakirci@outlook.com>
2026-08-29 13:27:40 +0300

    mobilde hamburger menü eklendi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.zenparke.com.tr',
   integrations: [sitemap()],
+  image: {
+    service: passthroughImageService(),
+  },
   devToolbar: {
     enabled: false,
   },
 });
+
